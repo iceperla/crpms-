f1f39ab9be2eb153b5c98356780ba217f0e5bcff
iceperla <161293358+iceperla@users.noreply.github.com>
2026-04-01 13:33:32 -0700

    Refactor MySQL connection code in db.js

--- config/db.js
+++ config/db.js
@@ -1,18 +1,17 @@
 const mysql = require('mysql2');
 
-// Connect to your running MySQL database
+
 const db = mysql.createConnection({
   host: 'localhost',
-  user: 'root',        // default XAMPP user
-  password: '',        // default is empty
-  database: 'crpms'    // make sure this database exists
-});
+  user: 'root',        
+  password: '',        
+  database: 'crpms'    
 
-// Attempt connection
+
 db.connect((err) => {
   if (err) {
     console.error('MySQL connection failed:', err.message);
-    process.exit(1); // stop the server if DB fails
+    process.exit(1); 
   }
   console.log('MySQL database connected successfully');
 });
