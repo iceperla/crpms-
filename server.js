adbcdbc16d3bd798378e825201bae657ad23e882
iceperla <161293358+iceperla@users.noreply.github.com>
2026-04-01 13:34:18 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const db = require('./config/db'); // initialize DB
+const db = require('./config/db'); 
 
 const app = express();
 
@@ -11,9 +11,9 @@ app.use(cors({
 
 app.use(express.json());
 
-// Simple test route to check server and DB
+
 app.get('/', (req, res) => {
-  // Example test query to MySQL
+  
   db.query('SELECT 1 + 1 AS solution', (err, results) => {
     if (err) {
       return res.status(500).send('Database test query failed');
@@ -22,6 +22,6 @@ app.get('/', (req, res) => {
   });
 });
 
-// Start the server
+
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
